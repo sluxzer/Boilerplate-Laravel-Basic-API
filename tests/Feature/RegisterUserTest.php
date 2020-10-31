42964e748b7bbddbd1a4223c8a149e418927fc95
taufik <initaufikhidayat@gmail.com>
2020-10-31 13:32:27 +0700

    Test for register

---
+++ tests/Feature/RegisterUserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class RegisterUserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_a_user_is_guest(){
+        $this->assertGuest();
+    }
+
+    public function test_register_new_user(){
+        $response = $this->withHeaders(['Accept' => 'application/json'])
+            ->post('/api/register', [
+            'name' => 'John',
+            'username' => 'johndoe',
+            'password' => 'test',
+            'email' => 'John@gmail.com'
+        ]);
+
+        $response->dump();
+
+        $response->
+
+//        $response->assertOk();
+        $response->assertJson([
+            'status' => true,
+            'message' => 'Register user successfully'
+        ]);
+    }
+}
