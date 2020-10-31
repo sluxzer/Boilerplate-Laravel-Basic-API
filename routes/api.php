f9a59d3a7d577069f2ff16b29a0477127958e070
taufik <initaufikhidayat@gmail.com>
2020-10-31 13:33:35 +0700

    remove unused api

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::middleware(['guest'])->group(function (){
     Route::post('/login', 'Auth\AuthController@login')->name('login');
     Route::post('/register', 'Auth\RegisterController@register');
